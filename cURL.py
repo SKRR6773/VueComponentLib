d307896a145e5f1f6eb99eebb3d7d7964b563a12
SKRR6773 <99122577+SKRR6773@users.noreply.github.com>
2023-12-07 16:09:47 +0700

    2023-12-07 16:09:47

--- cURL.py
+++ cURL.py
@@ -10,7 +10,9 @@ def calc_progress(full=100, curp=0):
     except ZeroDivisionError:
         return "###############"
 
-req = urllib.request.Request(r"http://localhost:9000/api/issues/search?componentKeys=VueComponentLib&s=FILE_LINE&resolved=false&ps=100&facets=severities%2Ctypes&additionalFields=_all&timeZone=Asia%2FBangkokmd", headers={})
+req = urllib.request.Request(r"http://localhost:9000/api/issues/search?componentKeys=VueComponentLib&s=FILE_LINE&resolved=false&ps=100&facets=severities%2Ctypes&additionalFields=_all&timeZone=Asia%2FBangkokmd", headers={
+    "": "XSRF-TOKEN=glk6gobkurnq1n9aqpd8vulul8; JWT-SESSION=eyJhbGciOiJIUzI1NiJ9.eyJsYXN0UmVmcmVzaFRpbWUiOjE3MDE5Mzk5MDU4MzgsInhzcmZUb2tlbiI6ImdsazZnb2JrdXJucTFuOWFxcGQ4dnVsdWw4IiwianRpIjoiQVl4Q2NiSFd2bkZpVC1tV2pLclgiLCJzdWIiOiJBWXdWT3l6b2pXRy1vb2VFdHN3XyIsImlhdCI6MTcwMTkyMTc5NiwiZXhwIjoxNzAyMTk5MTA1fQ.x9GWGDfCc4M6zeMcT7SgTAETxyoR7OqJU80mG7-hl4w"
+})
 
 reader = urllib.request.urlopen(req)
 
