f521b47645361e17b07e664e8e1ec20a8d676e76
SKRR6773 <99122577+SKRR6773@users.noreply.github.com>
2023-12-07 15:36:02 +0700

    2023-12-07 15:36:02

--- cURL.py
+++ cURL.py
@@ -4,5 +4,5 @@ import urllib.parse
 
 req = urllib.request.Request("http://mrg-web01.mrgshrimp.local/GA_SERVICES/translate/documentation/UNIT-3.pdf", )
 
-reader
+reader = urllib.parse.ur
 print(req)
