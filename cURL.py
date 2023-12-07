09f11ab85c3b2908c3229a627c96b5d2fb4795aa
SKRR6773 <99122577+SKRR6773@users.noreply.github.com>
2023-12-07 15:45:21 +0700

    2023-12-07 15:45:21

--- cURL.py
+++ cURL.py
@@ -2,6 +2,9 @@ import urllib.request
 import urllib.parse
 
 
+
+def calc_progress(full=100, )
+
 req = urllib.request.Request("http://mrg-web01.mrgshrimp.local/GA_SERVICES/translate/documentation/UNIT-3.pdf", )
 
 reader = urllib.request.urlopen(req)
