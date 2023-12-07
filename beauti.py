1cb5923b19a2f4a7a3778fb48ec078623c5afeaf
SKRR6773 <99122577+SKRR6773@users.noreply.github.com>
2023-12-07 16:13:49 +0700

    2023-12-07 16:13:49

--- beauti.py
+++ beauti.py
@@ -6,5 +6,9 @@ import json
 load_data = None
 
 with open(sys.argv[1], 'r')as fr:
-    with open(sys.argv[1], 'w')as fw:
-        json.dump(json.loads(fr.read()), fw, indent=4)
+    load_data = fr.read()
+
+
+
+with open(sys.argv[1], 'w')as fw:
+    json.dump(json.loads(fr.read()), fw, indent=4)
