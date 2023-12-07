93f439656b3c55a9cff1c8a7593ad551980de20c
SKRR6773 <99122577+SKRR6773@users.noreply.github.com>
2023-12-07 15:38:20 +0700

    2023-12-07 15:38:20

--- cURL.py
+++ cURL.py
@@ -5,7 +5,7 @@ import urllib.parse
 req = urllib.request.Request("http://mrg-web01.mrgshrimp.local/GA_SERVICES/translate/documentation/UNIT-3.pdf", )
 
 reader = urllib.request.urlopen(req)
-print(reader)
+print(reader.)
 
 with open("output.pdf", 'wb')as f:
     f.write(reader.read())
