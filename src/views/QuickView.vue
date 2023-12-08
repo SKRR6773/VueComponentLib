17cd3e2e086609132fbf98a8bcf63a6c65b77b79
Wiraphat Somkham (IT) <99122577+SKRR6773@users.noreply.github.com>
2023-12-08 11:34:36 +0700

    2023-12-08 11:34:36

--- src/views/QuickView.vue
+++ src/views/QuickView.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        <h1>st</h1>
+        <h1 class="mb-3"><strong>Quick Start!</strong></h1>
+
+        <section>
+            <header>
+                <h2>#1 </h2>
+            </header>
+        </section>
     </div>
 </template>
